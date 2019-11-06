2f82a4a47bd35215526ba8983c9756fd5e0c21bc
Louis <yia0223@gmail.com>
2019-11-06 18:11:34 +0800

    ~

---
+++ docxtohtml/src/app/_docx/index.ts
@@ -0,0 +1,1 @@
+export const docxjs = globalThis.require('docx4js').docx;
--- docxtohtml/src/app/dt/dt.component.ts
+++ docxtohtml/src/app/dt/dt.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {docxjs } from '../_docx';
 
 @Component({
   selector: 'app-dt',
@@ -10,14 +11,19 @@ export class DtComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    console.log(docxjs);
   }
 
 
   test(e) {
     const file = e.target.files[0];
-    window.require('docx4js').docx.load(file)
+    docxjs.load(file)
       .then(docx => {
-        console.log(docx);
+        docx.render((type, props, children) => {
+          console.log(type);
+          console.log(props);
+          console.log(children);
+        });
       });
     e.target.value = null;
   }

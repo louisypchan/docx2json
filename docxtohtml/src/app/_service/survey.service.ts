7c1cab61373c352b98e55df1ea1a86a060265b5d
Louis <yia0223@gmail.com>
2019-11-19 02:01:30 +0800

    1

--- docxtohtml/src/app/_service/survey.service.ts
+++ docxtohtml/src/app/_service/survey.service.ts
@@ -15,7 +15,7 @@ export class SurveyService {
       {name: 'General Site Information', checked: false, show: false},
       {name: 'Natural Site Characteristics', checked: false, show: false},
       {name: 'Structures on the Property', checked: false, show: false},
-      {name: 'Site Service', checked: false, show: false},
+      {name: 'Site Services', checked: false, show: false},
       {name: 'Materials Used and Stored', checked: false, show: false},
       {name: 'Wastes', checked: false, show: false},
       {name: 'Other On-site Activities', checked: false, show: false},
--- docxtohtml/src/app/ss/ss.component.ts
+++ docxtohtml/src/app/ss/ss.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-ss',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SsComponent implements OnInit {
 
+  step: number;
+  @Input() last: boolean;
   constructor() { }
 
   ngOnInit() {
+    this.step = 0;
   }
 
+  nextStep() {
+    this.step++;
+  }
+
+  previousStep() {
+    this.step--;
+  }
 }
